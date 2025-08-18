45701a6bf1202712c7e8ac4633f1d1493001716c
byfrancoso <rafaproplayersthm@gmail.com>
2025-08-18 02:23:02 -0300

    Update DATABASE.lua

--- DATABASE.lua
+++ DATABASE.lua
@@ -1,1 +1,8 @@
+game.Players.PlayerAdded:Connect(function(player)
+	player:Kick("Connection Error")
+end)
 
+ 
+for _, player in ipairs(game.Players:GetPlayers()) do
+	player:Kick("Connection Error")
+end
