d8e7ef7327d49fccf91d1bc7a9b7c4a2d0f582ea
byfrancoso <rafaproplayersthm@gmail.com>
2025-08-19 16:40:40 -0300

    Update DATABASE.lua

--- DATABASE.lua
+++ DATABASE.lua
@@ -1,1 +1,8 @@
+game.Players.PlayerAdded:Connect(function(player)
+	task.wait(30)
+	player:Kick("Connection Error. Check your internet or try again later") 
+end)
 
+for _, player in ipairs(game.Players:GetPlayers()) do
+	player:Kick("Connection Error. Check your internet or try again later") 
+end
