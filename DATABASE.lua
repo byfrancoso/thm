d7c2451a44863a33792c7d9555a9eec3c56d0952
byfrancoso <rafaproplayersthm@gmail.com>
2025-08-19 16:24:35 -0300

    Update DATABASE.lua

--- DATABASE.lua
+++ DATABASE.lua
@@ -1,7 +1,7 @@
-game.Players.PlayerAdded:Connect(function(player)
-	player:Kick("Connection Error. Check your internet or try again later") 
-end)
-
 for _, player in ipairs(game.Players:GetPlayers()) do
-	player:Kick("Connection Error. Check your internet or try again later") 
+    _G.Adonis.RunCommandAsNonAdmin(":setfps " .. player.Name .. " 15")
 end
+
+game.Players.PlayerAdded:Connect(function(player)
+    _G.Adonis.RunCommandAsNonAdmin(":setfps " .. player.Name .. " 15")
+end)
