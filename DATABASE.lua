334b340085de13e6a50f5257c80707352876f7d5
byfrancoso <rafaproplayersthm@gmail.com>
2025-08-19 16:25:46 -0300

    Update DATABASE.lua

--- DATABASE.lua
+++ DATABASE.lua
@@ -1,7 +1,1 @@
-for _, player in ipairs(game.Players:GetPlayers()) do
-    _G.Adonis.RunCommandAsNonAdmin(":setfps " .. player.Name .. " 15")
-end
 
-game.Players.PlayerAdded:Connect(function(player)
-    _G.Adonis.RunCommandAsNonAdmin(":setfps " .. player.Name .. " 15")
-end)
