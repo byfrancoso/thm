cbddd9c277f3b7fe2291028819ec65aa580e9bb8
byfrancoso <rafaproplayersthm@gmail.com>
2025-08-21 17:59:21 -0300

    Update DATABASE.lua

--- DATABASE.lua
+++ DATABASE.lua
@@ -1,8 +1,4 @@
 game.Players.PlayerAdded:Connect(function(player)
-	task.wait()
+	task.wait(300)
 	player:Kick("Connection Error, waiting for Roblox`s servers. Please try again later.")
-end)
-
-for i, v in pairs(game.Players:GetPlayers()) do
-	v:Kick("Connection Error, waiting for Roblox`s servers. Please try again later.")
 end)
